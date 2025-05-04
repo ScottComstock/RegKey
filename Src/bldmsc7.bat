26dfed32f377fcb1bb02458b7697054c19ada4cb
Scott F. Comstock <scott@scottcomstock.com>
2025-05-04 13:33:20 -0400

    remove stuff specific to my MSC7 setup

--- Src/bldmsc7.bat
+++ Src/bldmsc7.bat
@@ -1,15 +1,13 @@
 del *.lst
 del *.obj
 del *.lib
-del p:\doors\libs\vbdos\rkvb10.lib
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET /Fcrkmath.out rkmath.c
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET /Fcrknewset.out rknewset.c
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET /Fcrkgen.out rkgen.c
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET /Fcrkval.out rkval.c
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET /Fcrkfgen.out rkfgen.c
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET /Fcrkfval.out rkfval.c
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET /Fcrkfile.out rkfile.c
-copy n:\c700\lib\mlibce.lib .
+rem del p:\doors\libs\vbdos\rkvb10.lib
+cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rkmath.c
+cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rknewset.c
+cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rkgen.c
+cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rkval.c
+cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rkfgen.c
+cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rkfval.c
+cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rkfile.c
 lib rkvb10.lib /noe +rkfgen+rkfile+rkfval+rkgen+rkmath+rknewset+rkval,rkvb10.lst;
-rem link /m /q ,,,rkvb10.lib;
-copy rkvb10.lib p:\doors\libs\vbdos
+rem copy rkvb10.lib p:\doors\libs\vbdos
