81792b95d1bd80a038de8ac35908ccb4febb3e49
Scott F. Comstock <scott@scottcomstock.com>
2025-05-10 14:36:44 -0400

    makefile added

--- Src/bldmsc7.bat
+++ Src/bldmsc7.bat
@@ -1,13 +1,16 @@
 del *.lst
 del *.obj
 del *.lib
+del *.ASM
+del *.QLB
 rem del p:\doors\libs\vbdos\rkvb10.lib
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rkmath.c
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rknewset.c
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rkgen.c
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rkval.c
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rkfgen.c
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rkfval.c
-cl /c /Gq /o /DBUILDING_REGKEY /DNOT_C_TARGET /DVB_TARGET rkfile.c
-lib rkvb10.lib /noe +rkfgen+rkfile+rkfval+rkgen+rkmath+rknewset+rkval,rkvb10.lst;
+cl /c /AL /Gt8 /Ge /FaRKMATH.ASM /DNOT_C_TARGET /DVB_TARGET rkmath.c
+cl /c /AL /Gt8 /Ge /FaRKNEWSET.ASM /DNOT_C_TARGET /DVB_TARGET rknewset.c
+cl /c /AL /Gt8 /Ge /FaRKGEN.ASM /DNOT_C_TARGET /DVB_TARGET rkgen.c
+cl /c /AL /Gt8 /Ge /FaRKVAL.ASM /DNOT_C_TARGET /DVB_TARGET rkval.c
+cl /c /AL /Gt8 /Ge /FaRKFGEN.ASM /DNOT_C_TARGET /DVB_TARGET rkfgen.c
+cl /c /AL /Gt8 /Ge /FaRKFVAL.ASM /DNOT_C_TARGET /DVB_TARGET rkfval.c
+cl /c /AL /Gt8 /Ge /FaRKFILE.ASM /DNOT_C_TARGET /DVB_TARGET rkfile.c
+lib rkvb10.lib +rkfgen+rkfile+rkfval+rkgen+rkmath+rknewset+rkval,rkvb10.lst;
+link /nod /q /packc /packd /packf rkfgen+rkfile+rkfval+rkgen+rkmath+rknewset+rkval,rkvb10.qlb,,n:\vbdos10\lib\vbdosqlb.lib;
 rem copy rkvb10.lib p:\doors\libs\vbdos
---
+++ Src/msc7.mak
@@ -0,0 +1,38 @@
+CC=cl
+CFLAGS=/c /AL /nologo /W3
+LFLAGS=/Q
+OBJS=rkgen.obj rkval.obj rknewset.obj rkfgen.obj rkfval.obj rkfile.obj rkmath.obj
+LFLAGSOLD=/Q /NOE /DOSSEG /PACKC /PACKD
+all: rkvb10.lib rkvb10.qlb
+
+rkvb10.lib: $(OBJS)
+    lib rkvb10.lib +rkfgen+rkfile+rkfval+rkgen+rkmath+rknewset+rkval,rkvb10.lst;
+
+rkvb10.qlb: $(OBJS)
+    link $(LFLAGS) +rkfgen+rkfile+rkfval+rkgen+rkmath+rknewset+rkval,rkvb10.qlb,,n:\vbdos10\lib\vbdosqlb.lib;
+    
+rkgen.obj: rkgen.c regkey.h rkintern.h
+    $(CC) $(CFLAGS) rkgen.c
+
+rkval.obj: rkval.c regkey.h rkintern.h
+    $(CC) $(CFLAGS) rkval.c
+
+rknewset.obj: rknewset.c regkey.h rkintern.h
+    $(CC) $(CFLAGS) rknewset.c
+
+rkfgen.obj: rkfgen.c regkey.h rkintern.h
+    $(CC) $(CFLAGS) rkfgen.c
+
+rkfval.obj: rkfval.c regkey.h rkintern.h
+    $(CC) $(CFLAGS) rkfval.c
+
+rkfile.obj: rkfile.c regkey.h rkintern.h
+    $(CC) $(CFLAGS) rkfile.c
+
+rkmath.obj: rkmath.c regkey.h rkintern.h
+    $(CC) $(CFLAGS) rkmath.c
+
+clean:
+    del *.obj
+    del *.lib
+    del *.qlb
